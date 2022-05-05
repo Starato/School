9af97f84bc3e3d6303fd9a71bf9cba89d4babdec
Starato <92694528+Starato@users.noreply.github.com>
2022-05-05 00:27:35 -0500

    Add files via upload

---
+++ 2040/helloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// int a = 5;
+// int b = 4;
+// int c = 2;
+// int d = a+b * c;
+// int e = (a+b) * c;
+// Console.WriteLine(d);
+// Console.WriteLine(e);
+
+string aFriend = "bill";
+aFriend = "Maira";
+Console.WriteLine("hello"+aFriend);
